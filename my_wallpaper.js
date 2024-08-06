b1b199b238875335aff7bfc3fa362362bb9b1234
Lizzieworth <wortheliz@myvuw.ac.nz>
2024-08-06 15:24:05 +1200

    final submission

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,23 +1,46 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+
+let cherryLX=40
+let cherryLY=50
 
 
+
+
+ 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(NINE_LANDSCAPE);
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_width  = 100;
+  pWallpaper.grid_settings.cell_height = 100;
   pWallpaper.grid_settings.row_offset  = 50;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(245, 83, 199); //pink
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+ 
+  let cherryLX=40 
+  let cherryLY=50
+
+  stroke (40, 84, 43) // green for stem stroke
+  fill (201, 12, 22) //red fill for cherries
+  
+  line (cherryLX, cherryLY, cherryLY, cherryLX-20) // cherry stem left
+  line (cherryLY+10, cherryLY, cherryLY, cherryLX-20) // cherry stem right 
+  
+  
+  fill (201, 12, 22) //red fill for cherries
+  stroke (201, 12, 22) // red stroke for cherries
+  ellipse (cherryLX,cherryLY,cherryLX-20, cherryLX-20); // cherry left
+  ellipse (cherryLY+10,cherryLY,cherryLX-20, cherryLX-20); // cherry right 
+
 }
+
+
+
+
